71801a6206643439b53b6ad68c1dfd032cfdf2b1
Zhenya104 <37476377+Zhenya104@users.noreply.github.com>
2021-01-09 20:07:03 +0300

    Add files via upload

---
+++ laba_05_04_06.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+#include "Product.h"
+#include "Factory.h"
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	MilitaryEquipment* arr[5];
+	IMilitaryEquipmentFactory* Factory;
+	Factory = new RuMilitaryEquipmentFactory("RuMilitary.txt");
+	arr[0] = Factory->createTank();
+	arr[1] = Factory->createTroopCarrier();
+	arr[2] = Factory->createRobot();
+	arr[3] = Factory->createHowitzer();
+	arr[4] = Factory->createFighter();
+	
+	if (Factory) delete Factory;
+}
